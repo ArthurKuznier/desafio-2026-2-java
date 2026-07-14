3ed8cfc7781d1a6ddd207052c96cc0fc522b3604
ArthurKuznier <kuznierprog@gmail.com>
2026-07-14 19:52:41 -0300

    feat: adiciona validacao de status (RF03)

--- src/main/java/br/edu/unoesc/gestao_documentos/service/SolicitacaoService.java
+++ src/main/java/br/edu/unoesc/gestao_documentos/service/SolicitacaoService.java
@@ -1,19 +1,59 @@
 package br.edu.unoesc.gestao_documentos.service;
 
-import org.springframework.stereotype.Service;
-import br.edu.unoesc.gestao_documentos.repositories.SolicitacaoRepository;
 import br.edu.unoesc.gestao_documentos.domain.Solicitacao;
+import br.edu.unoesc.gestao_documentos.domain.Status;
+import br.edu.unoesc.gestao_documentos.repositories.SolicitacaoRepository;
+import br.edu.unoesc.gestao_documentos.repositories.StatusRepository;
+import org.springframework.stereotype.Service;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class SolicitacaoService {
     private final SolicitacaoRepository solicitacaoRepository;
+    private final StatusRepository statusRepository;
 
-    public SolicitacaoService(SolicitacaoRepository solicitacaoRepository) {
+    public SolicitacaoService(SolicitacaoRepository solicitacaoRepository, StatusRepository statusRepository) {
         this.solicitacaoRepository = solicitacaoRepository;
+        this.statusRepository = statusRepository;
     }
 
     // Insert pelo JPA
     public Solicitacao criarSolicitacao(Solicitacao solicitacao) {
         return this.solicitacaoRepository.save(solicitacao);
+    }
+
+    public Solicitacao alterarStatus(Integer solicitacaoId, Integer novoStatusId, Integer responsavelInformado) {
+        Solicitacao solicitacao = solicitacaoRepository.findById(solicitacaoId)
+                .orElseThrow(() -> new EntityNotFoundException("Solicitação não encontrada"));
+
+        Status novoStatus = statusRepository.findById(novoStatusId)
+                .orElseThrow(() -> new EntityNotFoundException("Status não encontrado"));
+
+        if (novoStatus.getResponsavel() != responsavelInformado) {
+            throw new RuntimeException("Responsavel Invalido");
+        }
+
+        String statusAtualNome = solicitacao.getStatus().getNome().toUpperCase();
+        String novoStatusNome = novoStatus.getNome().toUpperCase();
+
+        if (!validarTransicao(statusAtualNome, novoStatusNome)) {
+            throw new RuntimeException("Transição inválida de " + statusAtualNome + " para " + novoStatusNome);
+        }
+
+        solicitacao.setStatus(novoStatus);
+        return solicitacaoRepository.save(solicitacao);
+    }
+
+    private boolean validarTransicao(String atual, String novo) {
+        if (atual.equals("ABERTA") && !novo.equals("EM ANALISE"))
+            return true;
+        if (atual.equals("EM_ANALISE") && !novo.equals("REPROVADA"))
+            return true;
+        if (atual.equals("EM_ANALISE") && novo.equals("APROVADA"))
+            return true;
+        if (atual.equals("APROVADA") && !novo.equals("EMITIDA"))
+            return true;
+
+        return false;
     }
 }
