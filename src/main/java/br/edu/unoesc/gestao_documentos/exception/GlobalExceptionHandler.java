391421a451851f63cc62f70b4b42be117e1e9e41
ArthurKuznier <kuznierprog@gmail.com>
2026-07-15 21:34:34 -0300

    refactor: separa RegraNegocioException do RuntimeException generico no exception handler

--- src/main/java/br/edu/unoesc/gestao_documentos/exception/GlobalExceptionHandler.java
+++ src/main/java/br/edu/unoesc/gestao_documentos/exception/GlobalExceptionHandler.java
@@ -2,16 +2,23 @@ package br.edu.unoesc.gestao_documentos.exception;
 
 import br.edu.unoesc.gestao_documentos.dto.ErroResponseDto;
 import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErroResponseDto> handleRegraNegocioException(RuntimeException ex) {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(RegraNegocioException.class)
+    public ResponseEntity<ErroResponseDto> handleRegraNegocio(RegraNegocioException ex) {
         ErroResponseDto erro = new ErroResponseDto(
                 HttpStatus.BAD_REQUEST.value(),
                 "Erro de Validação",
@@ -20,11 +27,33 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErroResponseDto> handleNotFoundException(EntityNotFoundException ex) {
+    public ResponseEntity<ErroResponseDto> handleNotFound(EntityNotFoundException ex) {
         ErroResponseDto erro = new ErroResponseDto(
                 HttpStatus.NOT_FOUND.value(),
                 "Recurso Não Encontrado",
                 ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErroResponseDto> handleValidacaoDeCampos(MethodArgumentNotValidException ex) {
+        String mensagem = ex.getBindingResult().getFieldErrors().stream()
+                .map(erroDeCampo -> erroDeCampo.getField() + ": " + erroDeCampo.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+        ErroResponseDto erro = new ErroResponseDto(
+                HttpStatus.BAD_REQUEST.value(),
+                "Erro de Validação de Campos",
+                mensagem);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErroResponseDto> handleErroInesperado(Exception ex) {
+        log.error("Erro inesperado não tratado", ex);
+        ErroResponseDto erro = new ErroResponseDto(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Erro Interno",
+                "Ocorreu um erro inesperado. Tente novamente mais tarde.");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erro);
     }
 }
---
+++ src/main/java/br/edu/unoesc/gestao_documentos/exception/RegraNegocioException.java
@@ -0,0 +1,8 @@
+package br.edu.unoesc.gestao_documentos.exception;
+
+public class RegraNegocioException extends RuntimeException {
+
+    public RegraNegocioException(String mensagem) {
+        super(mensagem);
+    }
+}
